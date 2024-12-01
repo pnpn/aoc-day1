4c59e16cfd3cb7141c5e4f5757854fb311c1ae74
Pierre-Nicolas FALCOZ <pnfalcoz@tilakhealthcare.com>
2024-12-01 13:40:09 +0100

    Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
 use std::fs::read_to_string;
 
-fn distance(a: u64, b: u64) -> u64 {
-    match a.cmp(&b) {
-        std::cmp::Ordering::Less => b - a,
-        std::cmp::Ordering::Greater => a - b,
-        std::cmp::Ordering::Equal => 0,
-    }
-}
-
 fn main() {
     let mut left_list: Vec<u64> = Vec::new();
     let mut right_list: Vec<u64> = Vec::new();
-    let mut distances: Vec<u64> = Vec::new();
+    let mut occurences: Vec<u64> = Vec::new();
 
     for line in read_to_string("./input.txt").unwrap().lines() {
         for (index, part) in line.split_whitespace().enumerate() {
@@ -23,15 +15,12 @@ fn main() {
         }
     }
 
-    left_list.sort();
-    right_list.sort();
-
-    let it = left_list.iter().zip(right_list.iter());
-
-    for (left, right) in it {
-        distances.push(distance(*left, *right));
+    for num in left_list.iter() {
+        let occurence = right_list.iter().filter(|i| **i == *num).count();
+        occurences.push(num * (occurence as u64));
     }
 
-    let res: u64 = distances.iter().sum();
+    let res: u64 = occurences.iter().sum();
+
     print!("{}", res)
 }
